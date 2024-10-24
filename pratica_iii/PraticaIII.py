e02cdc4ee4a53998d5c2e6a7f8d91ba43e46c9d3
Augustus <guiolivera2004@hotmail.com>
2024-10-23 21:42:26 -0300

    update (falta 3 questões)

--- pratica_iii/PraticaIII.py
+++ pratica_iii/PraticaIII.py
@@ -11,18 +11,23 @@ def PSNR(original,decodificada,b):
         return "Infinito"
 
 def MSE(ori, dec):
+    mse = 0
     nsymbols = ori.width * ori.height * 3
     for i in range(ori.width):
         for j in range(ori.height):
             ori_r, ori_g, ori_b = ori.getpixel((i, j))
             dec_r, dec_g, dec_b = dec.getpixel((i, j))
-    return 0
+            mse += (ori_r - dec_r) ** 2
+            mse += (ori_g - dec_g) ** 2
+            mse += (ori_b - dec_b) ** 2
+    mse /= nsymbols
+    return mse
 
 if __name__ == "__main__":
     # filepath = '/content/drive/MyDrive/PraticaIII/lena.bmp'
     filepath = 'peixe.bmp'
     img = Image.open(filepath)
-    matriculas = [20205642]
+    matriculas = [22100621, 23250860, 22200378]
     
     # instancia objeto Cuif, convertendo imagem em CUIF.1
     cuif = Cuif(img,1,matriculas)
@@ -35,15 +40,18 @@ if __name__ == "__main__":
     
     #gera o arquivo Cuif.1
     cuif.save('peixe1.cuif')
+    filepathdec = 'peixe1.cuif'
     
     #Abre um arquivo Cuif e gera o objeto Cuif
-    #cuif1 = Cuif.openCUIF('peixe1.cuif')
+    cuif1 = Cuif.openCUIF('peixe1.cuif')
+
     
     # Converte arquivo Cuif em BMP e mostra
-    #cuif1.saveBMP("peixe1.bmp")
-    #cuif1.show()
+    cuif1.saveBMP("peixe1.bmp")
+    cuif1.show()
+    filepathdec = 'peixe1.bmp'
     
-    #img1 = Image.open("peixe1.bmp")
+    img1 = Image.open(filepathdec)
 
-    #psnr = PSNR(img, img1, 8)
-    #print(f'Cálculo do PSNR: {psnr}')
+    psnr = PSNR(img, img1, 8)
+    print(f'Cálculo do PSNR: {psnr}')

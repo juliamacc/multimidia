f2c9b4bbf870e7b433a93a12300a34df1b1a113d
Guilherme Augusto <guiolivera2004@hotmail.com>
2024-10-24 17:16:58 -0300

    update, falta da 6 até 8

--- pratica_iii/PraticaIII.py
+++ pratica_iii/PraticaIII.py
@@ -10,7 +10,7 @@ def PSNR(original,decodificada,b):
     except ZeroDivisionError:
         return "Infinito"
 
-def MSE(ori, dec):
+def MSE(ori, dec): #Completo - Questão 3 do relatorio.
     mse = 0
     nsymbols = ori.width * ori.height * 3
     for i in range(ori.width):
@@ -22,6 +22,7 @@ def MSE(ori, dec):
             mse += (ori_b - dec_b) ** 2
     mse /= nsymbols
     return mse
+                    #Completo - Questão 3 do relatorio.
 
 if __name__ == "__main__":
     # filepath = '/content/drive/MyDrive/PraticaIII/lena.bmp'
@@ -30,7 +31,10 @@ if __name__ == "__main__":
     matriculas = [22100621, 23250860, 22200378]
     
     # instancia objeto Cuif, convertendo imagem em CUIF.1
-    cuif = Cuif(img,1,matriculas)
+    #cuif = Cuif(img,1,matriculas)
+
+    # instancia objeto Cuif, convertendo imagem em CUIF.2
+    cuif = Cuif(img,2,matriculas)
     
     # imprime cabeçalho Cuif
     cuif.printHeader()
@@ -39,17 +43,24 @@ if __name__ == "__main__":
     cuif.show()
     
     #gera o arquivo Cuif.1
-    cuif.save('peixe1.cuif')
-    filepathdec = 'peixe1.cuif'
-    
+    #cuif.save('peixe1.cuif')
+    #filepathdec = 'peixe1.cuif'
+    cuif.save('peixe2.cuif')
+    filepathdec = 'peixe2.cuif'
     #Abre um arquivo Cuif e gera o objeto Cuif
-    cuif1 = Cuif.openCUIF('peixe1.cuif')
+    #cuif1 = Cuif.openCUIF('peixe1.cuif')
 
+    cuif2 = Cuif.openCUIF('peixe2.cuif')
     
-    # Converte arquivo Cuif em BMP e mostra
-    cuif1.saveBMP("peixe1.bmp")
-    cuif1.show()
-    filepathdec = 'peixe1.bmp'
+    # Converte arquivo Cuif.1 em BMP e mostra
+    #cuif1.saveBMP("peixe1.bmp")
+    #cuif1.show()
+    #filepathdec = 'peixe1.bmp'
+
+    # Converte arquivo Cuif.2 em BMP e mostra
+    cuif2.saveBMP("peixe2.bmp")
+    cuif2.show()
+    filepathdec = 'peixe2.bmp'
     
     img1 = Image.open(filepathdec)
 
